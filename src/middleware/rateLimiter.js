fe712f83962ad94e149cd947efff750b6b377e53
UtkarshSingh4 <utkarsh.1018rajput@gmail.com>
2026-02-01 16:06:03 +0530

    rate

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const weatherLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // max 100 requests per IP
+  max: 100, // max 100 requests per IP
   standardHeaders: true,
   legacyHeaders: false,
   message: {
